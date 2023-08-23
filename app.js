15d96e86bffe20ad10cb4a1ed8413ef981337cf1
tinega chris <chrisantustinega@gmail.com>
2023-08-23 04:33:55 +0300

    change

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log('Serving on port 3000')
 })
